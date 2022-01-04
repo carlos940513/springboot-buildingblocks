60875cfccc9ef91ba69899644187e867e24b28fa
carlos940513 <juancarlos940513@gmail.com>
2022-01-04 10:59:00 -0600

    Second commit form CMD

--- src/main/java/com/stacksimplify/restservices/MasterResfulApplication.java
+++ src/main/java/com/stacksimplify/restservices/MasterResfulApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasterResfulApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(MasterResfulApplication.class, args);
 	}

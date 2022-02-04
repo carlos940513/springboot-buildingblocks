92fcf86ec636c0306dee81b6851b7f024793e50b
carlos940513 <juancarlos940513@gmail.com>
2022-02-04 16:19:17 -0600

    JPA-OneToMany-FirstCommit

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,6 +31,7 @@ import com.stacksimplify.restservices.services.UserService;
 //Controller 
 @RestController
 @Validated
+@RequestMapping(value="users")
 public class UserController {
 	
 	//Autowire the userService
@@ -37,7 +39,7 @@ public class UserController {
 	private UserService userService;
 	
 	//getAllUser Method
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getAllUsers(){
 		
 		return userService.getAllusers();
@@ -46,7 +48,7 @@ public class UserController {
 	//Create User
 	//@RequestBody Annotation
 	//@PostMapping Annotation
-	@PostMapping("/users")
+	@PostMapping
 	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, UriComponentsBuilder builder) {
 		try {
 			userService.createUser(user);
@@ -60,7 +62,7 @@ public class UserController {
 	}
 	
 	//getUserById
-	@GetMapping("/users/{id}")
+	@GetMapping("/{id}")
 	public Optional<User> getUserByID(@PathVariable("id") @Min(1) Long id){
 		try {
 			return userService.getUserById(id);
@@ -71,7 +73,7 @@ public class UserController {
 	}
 	
 	//updateUserById
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public User updateUserById(@PathVariable("id")Long id, @RequestBody User user) {
 		try {
 			return userService.updateUserById(id, user);
@@ -81,13 +83,13 @@ public class UserController {
 	}
 	
 	//deleteUserById
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUserById(@PathVariable("id") Long id){
 		userService.deleteUserById(id);
 	}
 	
 	//getUserByUsername
-	@GetMapping("/users/byusername/{username}")
+	@GetMapping("/byusername/{username}")
 	public User getUserByUsername(@PathVariable("username")String username) throws UserNameNotFoundException {
 		User user = userService.getUserByUsername(username);
 		
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-insert into user values(101,'jcem_01@gmail.com','jose','mora','jcem_01@gma.com','DBA','2020');
+insert into user (id,user_name,first_name,last_name,email_address,role,ssn) values(101,'jcem_01@gmail.com','jose','mora','jcem_01@gma.com','DBA','2020');
+insert into user (id,user_name,first_name,last_name,email_address,role,ssn) values(102,'elbarto@gmail.com','Bartolome','Simspon','barto@gma.com','DBA','2021');
+
+insert into orders (orderid,orderdescription,user_id) values(2001,'order11',101);
+insert into orders (orderid,orderdescription,user_id) values(2002,'order12',101);
+insert into orders (orderid,orderdescription,user_id) values(2003,'order13',101);
+insert into orders (orderid,orderdescription,user_id) values(2004,'order21',102);
+insert into orders (orderid,orderdescription,user_id) values(2005,'order22',102);
